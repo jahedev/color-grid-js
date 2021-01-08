b2d3cdd6edf6805772e918d0df4ce37f8aa880e8
jahedev <jahedev@gmail.com>
2021-01-07 23:00:17 -0500

    all add/remove rows and columns work

--- script.js
+++ script.js
@@ -16,23 +16,58 @@ var color = '#c23616'; // red
 /* #endregion */
 
 /* #region: Event Listeners */
+addCol.addEventListener('click', (e) => {
+  const rows = table.querySelectorAll('tr');
+  for (const row of rows) {
+    row.innerHTML += generateCols(1);
+  }
+});
+
+removeCol.addEventListener('click', (e) => {
+  const rows = table.querySelectorAll('tr');
+  for (const row of rows) {
+    const cols = row.querySelectorAll('td');
+    if (cols.length > 1) cols[cols.length - 1].remove();
+  }
+});
+
 addRow.addEventListener('click', (e) => {
-  const rows = numOfRows();
-  const cols = numOfCols();
-  const rowHTML = generateRow(cols);
+  const rowHTML = generateRow(numOfCols());
   table.innerHTML += rowHTML;
 });
 
 removeRow.addEventListener('click', (e) => {
   if (table.childElementCount > 1) table.lastChild.remove();
 });
+var x = 1;
+// table.addEventListener('mousedown', (e) => {
+//   if (e.target.tagName == 'TD') {
+//     // const td = e.target;
+//     // td.style.backgroundColor = color;
+//     e.target.onmousedown(function () {
+//       console.log('mouse is down: ', x++);
+//     });
+//   }
+// });
 
-table.addEventListener('mousedown', (e) => {
-  if (e.target.tagName == 'TD') {
-    const td = e.target;
-    td.style.backgroundColor = color;
-  }
-});
+var mouseDown = false;
+var mouseOver = false;
+
+table.onmouseover = function () {
+  mouseOver = true;
+};
+
+table.onmouseleave = function () {
+  mouseOver = false;
+};
+
+table.onmousedown = function () {
+  mouseDown = true;
+};
+table.onmouseup = function () {
+  mouseDown = false;
+};
+
 /* #endregion */
 /* #region: Function */
 function numOfCols(tableEl = table.lastChild) {
@@ -43,7 +78,10 @@ function numOfRows(tableEl = table) {
   return tableEl.querySelectorAll('tr').length;
 }
 function generateRow(colNum = 1) {
-  const cols = '<td>&nbsp</td>'.repeat(colNum);
-  return `<tr>${cols}</tr>`;
+  return `<tr>${generateCols(colNum)}</tr>`;
+}
+
+function generateCols(colNum = 1) {
+  return '<td>&nbsp</td>'.repeat(colNum);
 }
 /* #endregion */
