8c4b240f1bfe36c166bc0c91892ea488048e4023
jahedev <jahedev@gmail.com>
2021-01-07 21:10:19 -0500

    variables for elements

---
+++ script.js
@@ -0,0 +1,20 @@
+/* #region: Variables */
+
+const addCol = document.querySelector('#add-col-btn');
+const removeCol = document.querySelector('#remove-col-btn');
+
+const findAll = document.querySelector('#findall-btn');
+const findColored = document.querySelector('#findcolored-btn');
+const clearAll = document.querySelector('#clearall-btn');
+
+const addRow = document.querySelector('#add-row-btn');
+const removeRow = document.querySelector('#remove-row-btn');
+
+const table = document.querySelector('table');
+
+/* #endregion */
+
+/* #region: Event Listeners */
+/* #endregion */
+/* #region: Function */
+/* #endregion */
